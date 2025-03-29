e9f996b4d734b8371987349d005a1911b0833ee7
juju <juliapinheiroroque960@gmail.com>
2025-03-29 18:20:05 -0300

    Module 2 - Model and Configure Entity Framework

--- Program.cs
+++ Program.cs
@@ -5,20 +5,4 @@ app.MapGet("/", () => {
     return Results.Ok("Hello World"); // Return a 200 OK response
 }); 
 
-// Route with a parameter
-// url/name/Julia returns Hello Julia
-app.MapGet("name/{nome}", (string nome) => {
-    return Results.Ok($"Hello {nome}"); // Return a 200 OK response
-}); 
-
-app.MapPost("/", (User user) => { 
-    return Results.Ok(user); // Return a 200 OK response with the user object (JSON -> object: serialization; object -> JSON: deserialization)
-});
-
 app.Run(); // Run the web application
-
-public class User 
-{
-    public int Id { get; set; } 
-    public string Username { get; set; } = string.Empty; 
-}
